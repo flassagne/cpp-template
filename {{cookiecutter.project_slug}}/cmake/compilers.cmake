504f1bde81e432434a2045445f3710e76069b1d7
Fabien Lassagne <flassagne@unowhy.com>
2020-12-22 17:08:47 +0100

    🔊 active colored output for gcc and clang

--- {{cookiecutter.project_slug}}/cmake/compilers.cmake
+++ {{cookiecutter.project_slug}}/cmake/compilers.cmake
@@ -3,10 +3,20 @@ include_guard()
 # Define variables for each supported compiler, so that checking is easier
 # later.
 
+option(FORCE_COLORED_OUTPUT "Always produce ANSI-colored output (GNU/Clang only)." True)
+
 if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     set(USING_CLANG TRUE)
+
+    if (${FORCE_COLORED_OUTPUT})
+        add_compile_options (-fcolor-diagnostics)
+    endif()
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     set(USING_GCC TRUE)
+
+    if (${FORCE_COLORED_OUTPUT})
+        add_compile_options (-fdiagnostics-color=always)
+    endif()
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
     set(USING_MSVC TRUE)
 endif()
